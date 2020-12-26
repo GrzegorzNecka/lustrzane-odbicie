e55dce34b2d97a967d72464efbb557a551a1f2d8
GrzegorzNecka <strzypior@gmail.com>
2020-12-26 10:24:56 +0100

    change function name

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import "./style.css";
 const input = document.querySelector(".text-input"),
   mirror = document.querySelector(".mirror");
 
-const setMirror = e => {
+const setReflection = e => {
   const value = e.target.value;
   mirror.textContent = [...value].reverse().join("");
 };
 
-input.addEventListener("keyup", e => setMirror(e));
+input.addEventListener("keyup", e => setReflection(e));
