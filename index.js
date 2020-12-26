d7940487f6be91f27108c4d3e2ee3230bff924ea
GrzegorzNecka <strzypior@gmail.com>
2020-12-26 10:21:53 +0100

    deleted domContentLoaded event

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import "./style.css";
 
-document.addEventListener("DomContentLoaded", () => {
-  console.log("loaded");
-});
-
 const input = document.querySelector(".text-input"),
   mirror = document.querySelector(".mirror");
 
-
-
-const setMirror= e => {
+const setMirror = e => {
   const value = e.target.value;
   mirror.textContent = [...value].reverse().join("");
 };
